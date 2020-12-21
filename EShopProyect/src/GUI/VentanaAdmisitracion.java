d380784e9c78652d1bd8e656400d039925259e90
InigoTR <InigoTR@DESKTOP-229CNO0>
2020-12-21 12:31:00 +0100

    Creada ventana que invoca todos los metodos de administrador

---
+++ EShopProyect/src/GUI/VentanaAdmisitracion.java
@@ -0,0 +1,90 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import net.miginfocom.swing.MigLayout;
+
+public class VentanaAdmisitracion extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaAdmisitracion frame = new VentanaAdmisitracion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaAdmisitracion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[][][][][][][][][][][]", "[][][][][][][][]"));
+		
+		JButton btnNewButton = new JButton("AñadirProducto");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnNewButton, "cell 1 1");
+		
+		JButton btnNewButton_1 = new JButton("ModificarProducto");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnNewButton_1, "cell 3 1");
+		
+		JButton btnNewButton_2 = new JButton("EliminarProducto");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnNewButton_2, "cell 1 3");
+		
+		JButton btnNewButton_3 = new JButton("AñadirUsuario");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnNewButton_3, "cell 1 5");
+		
+		JButton btnNewButton_5 = new JButton("ModificarUsuario");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnNewButton_5, "cell 3 5");
+		
+		JButton btnNewButton_4 = new JButton("EliminarUsuario");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.add(btnNewButton_4, "cell 1 7");
+	}
+
+}

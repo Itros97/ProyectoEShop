8c0ec29e5b261acc5331202ac782379238dce91f
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-01-09 14:54:11 +0100

    Ventana Administracion mejorada

---
+++ EShopProyect/src/GUI/VentanaAdministracion.java
@@ -0,0 +1,74 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VentanaAdministracion {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaAdministracion window = new VentanaAdministracion();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaAdministracion() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JButton bcerrar = new JButton("Cerrar");
+		frame.getContentPane().add(bcerrar, BorderLayout.SOUTH);
+		bcerrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton busuario = new JButton("Usuario");
+		frame.getContentPane().add(busuario, BorderLayout.EAST);
+		busuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton bproducto = new JButton("Producto");
+		frame.getContentPane().add(bproducto, BorderLayout.WEST);
+		bproducto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+	
+	}
+
+}

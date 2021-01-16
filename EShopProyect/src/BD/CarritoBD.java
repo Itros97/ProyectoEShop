8593fcf168d1fd68e2dc3cb936918fa4b923e71e
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-01-16 01:05:04 +0100

    CLASE CARRITO INSERTADA EN LA bd, TODOS LOS ERRORES DE CODIGO EN SRC SOLUCIONADOS

--- EShopProyect/src/BD/CarritoBD.java
+++ EShopProyect/src/BD/CarritoBD.java
@@ -1,5 +1,27 @@
 package BD;
 
-public class CarritoBD {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+public class CarritoBD {
+	
+	 protected static void CrearTablaCarrito(Connection con) {
+			// TODO Auto-generated method stub
+		    PreparedStatement preparedStatement = null;
+		    //IDUsuario,nOMBREPRODUCTO E PRECIO
+		    //TABLA PRODUCTO
+		    String createcarrito = "CREATE TABLE CARRITO(" +
+		    		"IDCARRITO INTEGER PRIMARY KEY AUTO_INCREMENT," +
+		    		");";
+		    try {
+		    	
+		    preparedStatement = con.prepareStatement(createcarrito);
+	        preparedStatement.executeUpdate();
+
+	        System.out.println("Tabla Carrito creada correctamente.");
+				
+			} catch (Exception e) {
+				System.err.println("Error al crear la tabla" +e+ "");
+			}
+}
 }
